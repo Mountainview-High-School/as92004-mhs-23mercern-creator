b23dfe4214b35c2ebf9c9609dee7a99d65e65cb7
mhs-23mercern-creator <23mercern@mountainview.school.nz>
2026-05-18 12:03:05 +1200

    .

--- quiz.py
+++ quiz.py
@@ -4,26 +4,15 @@ while True:
         break
     except ValueError:
         print ("seems like you put a letter in there, please try again")
-
 if 8 <= age <= 13:
     print ("you are of age")
 else:
     exit((print ("you should try the cybrsmart youth quiz instead due to your age")))
-
-
 name = input("what is your name? ")
 print("hello, " + name + " welcome to the quiz.")
-
-
-
-
-
-
-
 guess = 0
 wrong_guess = 0 
 trys = 0
-
 while True:
     print("someone sends you a text that is hurtful and makes you feel bad about yourself. what should you do?")
     print("A) Delete the message and try to forget about it")
@@ -41,20 +30,15 @@ while True:
         wrong_guess += 1 
         trys += 1
     if trys >= 3:
-        print ( ""+name+ "you have used all your trys for this question the correct answer was B")
+        print ( ""+name+ " you have used all your trys for this question the correct answer was (B)")
         break
-
 if trys >= 3:
     trys = 0
-
 if wrong_guess >= 1:
     guess -= 1
     wrong_guess = 0
-
 if guess == -1:
     guess = 0
-
-
 while True:
     print("you want to join an online gaming site. which if the following unfirmation is okay for you to post on the site.")
     print("A) A nickname")
@@ -63,7 +47,7 @@ while True:
 
     choice = input("what one should you post? (A/B/C): ").strip().upper()
     if choice == "A":
-        print(f"correct " +name+ ". using a nickname is the safest option when joining an online gaming site")
+        print(f"correct " +name+ ". using a nickname is the safest option when joining an online gaming site.")
         guess += 1
         break
     else:
@@ -71,20 +55,15 @@ while True:
         wrong_guess += 1
         trys += 1
     if trys >= 3:
-        print ( ""+name+ "you have used all your trys for this question the correct answer was B")
+        print ( ""+name+ " you have used all your trys for this question the correct answer was (A)")
         break
-    
 if trys >= 3:
     trys = 0
-
 if wrong_guess >= 1:
     guess -= 1
     wrong_guess = 0
-
 if guess == -1:
     guess = 0
-
-
 while True:
     print("you find out that someone has posted an embarrassing picture of you online What should you do?")
     print("A) tweet that they are an idiot and a loser")
@@ -93,7 +72,7 @@ while True:
 
     choice = input("what would you do in this situation (A/B/C):").strip().upper()
     if choice == "C":
-        print(f"correct " +name+ ". telling an adult you trust is the best way to handle this situation")
+        print(f"correct " +name+ ". telling an adult you trust is the best way to handle this situation.")
         guess += 1
         break
     else:
@@ -101,19 +80,13 @@ while True:
         wrong_guess += 1    
         trys += 1
     if trys >= 3:
-        print ( ""+name+ "you have used all your trys for this question the correct answer was B")
+        print ( ""+name+ " you have used all your trys for this question the correct answer was (C)")
         break
-    
-       
-
 if trys >= 3:
     trys = 0
-
 if wrong_guess >= 1:
     guess -= 1
     wrong_guess = 0
-
 if guess == -1:
     guess = 0
-
-print(f"you got {guess} out of 3 questions correct. " +name+"!")
+print(f"you got {guess} out of 3 questions correct. " +name+"")
